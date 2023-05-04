c89f3fc8a1efc9da0888682db181fcc5dafada3c
LesbiansAttack <brooke.leichtle@gmail.com>
2023-05-04 11:52:17 +0200

    stuff

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -1,20 +1,22 @@
 use rosehip::{model, dataset};
+use std::time::Instant;
 use ndarray::Zip;
 
 fn main() {
     let mnist_dataset = dataset::Dataset::new_from_mnist(50_000, 10_000, 10_000);
-    let model = model::Model
+    let model = model::ModelBuilder
         ::default()
-        .add_layer(784, 128)
+        .add_linear_layer(784, 128)
         .add_sigmoid()
-        .add_layer(128, 10)
+        .add_linear_layer(128, 10)
         .add_softmax()
-        .build().unwrap();
+        .build()
+        .unwrap();
+    let now = Instant::now();
     Zip::from(mnist_dataset.training_data.rows())
         .and(mnist_dataset.training_labels.rows())
         .for_each(|data, label| { 
-                  println!("{}, {}", label, data);
-                  model.learn(data.to_owned(), label.get(0).unwrap());
-
+            model.forward_backward(data.to_owned(), label.get(0).unwrap());
         });
+    println!("took: {:.2?}", now.elapsed());
 }
